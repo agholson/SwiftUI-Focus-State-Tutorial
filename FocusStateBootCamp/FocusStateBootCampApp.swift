a2af315aac938645468e9f99667b1a5e0c852d98
agholson <87455899+agholson@users.noreply.github.com>
2022-02-12 12:49:35 -0500

    Initial Commit

---
+++ FocusStateBootCamp/FocusStateBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusStateBootCampApp.swift
+//  FocusStateBootCamp
+//
+//  Created by Leone on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FocusStateBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
